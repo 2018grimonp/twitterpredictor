447b6293b9171a7ead0f61ff9eb777167f229dd4
2018grimonp <pierrick.grimon@student-cs.fr>
2018-11-16 15:02:14 +0100

    courbe

--- collect/tweet_analysis.py
+++ collect/tweet_analysis.py
@@ -4,9 +4,15 @@ import pandas as pd
 
 import numpy as np
 
-def collect_to_pandas_dataframe():
-    connexion = twitter_setup()
-    tweets = connexion.search("@MLP_officiel",language="fr",rpp=100)
+connexion = twitter_setup()
+
+"""Objectif: La majorite des retweets sont fait par les followers. 
+On divise donc le nombre de retweets de deux candidats par le nombre de followers pour savoir quelle est
+la communaute la plus active. On remultiplie par 10 000 pour pouvoir visualiser.
+Rend plus ou moins bien en fonction de la periode de temps sur laquelle on se place"""
+
+def collect_to_pandas_dataframe(username): # ex: @EmmanuelMacron
+    tweets = connexion.search("username",language="fr",rpp=1000)
     data = pd.DataFrame(data=[tweet.text for tweet in tweets], columns=['tweet_textual_content'])
     data['len']  = np.array([len(tweet.text) for tweet in tweets])
     data['ID']   = np.array([tweet.id for tweet in tweets])
@@ -16,16 +22,22 @@ def collect_to_pandas_dataframe():
     data['RTs']    = np.array([tweet.retweet_count for tweet in tweets])
     return data
 
-data=collect_to_pandas_dataframe()
 
-def retweet():
-    column_rts=data['RTs']
-    return (column_rts)
+follower_count1 = connexion.get_user('JLMelenchon').followers_count
+follower_count2 = connexion.get_user('EmmanuelMacron').followers_count
+data1=collect_to_pandas_dataframe("@JLMelenchon")
+data2=collect_to_pandas_dataframe("@EmmanuelMacron")
 
-print(retweet())
+import matplotlib.pyplot as plt
 
-def likes():
-    column_like=data['Likes']
-    return (column_like)
+tret1 = pd.Series(data=data1['RTs'].values*10000/follower_count1, index=data1['Date'])
+tret2 = pd.Series(data=data2['RTs'].values*10000/follower_count2, index=data2['Date'])
+
+# Likes vs retweets visualization:
+tret1.plot(figsize=(16,4), label="Retweets JLM", legend=True)
+tret2.plot(figsize=(16,4), label="Retweets EM", legend=True)
+
+plt.show()
+
 
 
